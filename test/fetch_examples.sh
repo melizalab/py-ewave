0495b1de2fc6ba802dc126d5a05e5e165f608007
Dan Meliza <dan@meliza.org>
2021-02-22 17:07:10 -0500

    fix test fetch

--- test/fetch_examples.sh
+++ test/fetch_examples.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --output-dir test --remote-name-all https://meliza.org/software/py_ewave_testfiles/{M1F1-uint8-AFsp,M1F1-int16-AFsp,M1F1-int16WE-AFsp,M1F1-int32-AFsp,M1F1-int32WE-AFsp,M1F1-float32-AFsp,M1F1-float64WE-AFsp,6_Channel_ID}.wav
+cd test && curl --remote-name-all https://meliza.org/software/py_ewave_testfiles/{M1F1-uint8-AFsp,M1F1-int16-AFsp,M1F1-int16WE-AFsp,M1F1-int32-AFsp,M1F1-int32WE-AFsp,M1F1-float32-AFsp,M1F1-float64WE-AFsp,6_Channel_ID}.wav
